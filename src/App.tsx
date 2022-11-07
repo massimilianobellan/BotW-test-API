e1d90815985bd97f962b8ceb5d6fe6f03bdbbf18
Massimiliano Bellan <massimiliano.bellan@hydrogrid.domain>
2022-11-07 16:32:02 +0100

    Fixed prior mistake of deletion of file and finished Navbar Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import "./App.css";
 import Monsters from "./components/monster/Monsters";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { Navbar, Container } from "@nextui-org/react";
+import logo from "../src/assets/logo.png";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Navbar></Navbar>
-      </header>
+      <Navbar isBordered variant={"floating"}>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" width={100}></img>
+        </Navbar.Brand>
+        <Navbar.Content hideIn={"xs"}>
+          <Navbar.Link href="#">Monsters</Navbar.Link>
+          <Navbar.Link href="#">Equipment</Navbar.Link>
+          <Navbar.Link href="#">Materials</Navbar.Link>
+          <Navbar.Link href="#">Creatures</Navbar.Link>
+          <Navbar.Link href="#">Treasure</Navbar.Link>
+        </Navbar.Content>
+      </Navbar>
       <Container>
         <QueryClientProvider client={queryClient}>
           <Monsters />
